5ce08ea068886899d1f9531afc343d9668a8d83a
Dimitry <Kholod.ural@gmail.com>
2020-10-10 18:36:43 +0500

    added setup.py

---
+++ tc_ping/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='tc_ping',
+      version='1.0',
+      description='like ping but with tcp',
+      author='Kholod Dimitry kb-201',
+      author_email='qwerty@ytrewq.net',
+      url='qwerty.ytrewq',
+      packages=['tc_ping']
+     )
