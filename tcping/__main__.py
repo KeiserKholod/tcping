944fc40a8b6fe10d785d47d8326142376f85635f
Dimitry <Kholod.ural@gmail.com>
2020-11-03 17:29:12 +0500

    changed delay between pings

--- tcping/__main__.py
+++ tcping/__main__.py
@@ -5,6 +5,7 @@ from tcping import statistics
 import logging
 import time
 
+
 def create_cmd_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('destination', default=None,
@@ -15,7 +16,7 @@ def create_cmd_parser():
                         help='to set timeout')
     parser.add_argument('-p', '--port', default='80', dest='port',
                         help='to set port')
-    parser.add_argument('-d', '--delay', default='0', dest='delay',
+    parser.add_argument('-d', '--delay', default='0.5', dest='delay',
                         help='delay between pings in seconds')
     parser.add_argument('-6', '--ipv6', action='store_true', dest="use_ipv6",
                         help='to use ipv6')
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     tcp_ping = ping.TCPing(destination=args.destination, port=args.port,
                            timeout=args.timeout, use_ipv6=args.use_ipv6)
     pings_count = int(args.pings_count)
-    delay = int(args.delay)
+    delay = float(args.delay)
     output_level = int(args.output_level)
     try:
         i = 0
@@ -40,7 +41,8 @@ if __name__ == '__main__':
             measure = tcp_ping.do_ping()
             if output_level > 0:
                 print(tcp_ping.prepare_ping_info(measure))
-            time.sleep(delay)
+            if delay > measure:
+                time.sleep(delay - measure)
             i += 1
     except errors.PingError as e:
         logging.basicConfig(level=logging.INFO)
--- watchdog/__main__.py
+++ watchdog/__main__.py
@@ -12,7 +12,7 @@ def create_cmd_parser():
                         help='IP\'s, Domain\'s or URL\'s with ports')
     parser.add_argument('-t', '--timeout', default='0', dest='timeout',
                         help='to set timeout')
-    parser.add_argument('-d', '--delay', default='0', dest='delay',
+    parser.add_argument('-d', '--delay', default='0.5', dest='delay',
                         help='delay between pings in seconds')
     parser.add_argument('-6', '--ipv6', action='store_true', dest="use_ipv6",
                         help='to use ipv6')
@@ -34,12 +34,14 @@ def show_watchdog_tui(screen):
             destinations = watchdog_ping.WatchdogPingData.parse_destanations(args.destinations)
             watchdog_ping_data = watchdog_ping.WatchdogPingData(destinations=destinations,
                                                                 timeout=args.timeout, use_ipv6=args.use_ipv6)
-            delay = int(args.delay)
+            delay = float(args.delay)
             pings = watchdog_ping_data.get_pings()
             while True:
                 measures = []
                 for tcp_ping in pings:
                     measure = tcp_ping.do_ping()
+                    if delay > measure:
+                        time.sleep(delay - measure)
                     measure_with_destination = measure, tcp_ping.destination
                     measures.append(measure_with_destination)
                 table = watchdog_ping.WatchdogPingData.get_measures_to_print(measures)
@@ -54,7 +56,6 @@ def show_watchdog_tui(screen):
                 if ev in (ord('Q'), ord('q')):
                     return
                 screen.refresh()
-                time.sleep(delay)
         except errors.PingError as e:
             logging.basicConfig(level=logging.INFO)
             logging.error(e.message)
