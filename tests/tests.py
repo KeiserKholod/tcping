f8856e810670d419542e7b0db979802455576af5
Dimitry <Kholod.ural@gmail.com>
2020-11-17 17:02:24 +0500

    code was refactored according with pep8

--- tests/tests.py
+++ tests/tests.py
@@ -15,7 +15,7 @@ class AioTestCase(unittest.TestCase):
         self._function_cache = {}
         super(AioTestCase, self).__init__(methodName=method_name)
 
-    def coroutine_function_decorator(self, func):
+    def coro_func_decorator(self, func):
         def wrapper(*args, **kw):
             return self.loop.run_until_complete(func(*args, **kw))
 
@@ -25,7 +25,7 @@ class AioTestCase(unittest.TestCase):
         attr = object.__getattribute__(self, item)
         if asyncio.iscoroutinefunction(attr):
             if item not in self._function_cache:
-                self._function_cache[item] = self.coroutine_function_decorator(attr)
+                self._function_cache[item] = self.coro_func_decorator(attr)
             return self._function_cache[item]
         return attr
 
@@ -36,8 +36,7 @@ class TestCorrectPings(AioTestCase):
         args = cmd_parser.parse_args(['google.com'])
         tcp_ping = ping.TCPing(destination=args.destination, port=args.port,
                                timeout=args.timeout, use_ipv6=args.use_ipv6)
-        pings_count = int(args.pings_count)
-        one_measure = await tcp_ping.do_ping()
+        await tcp_ping.do_ping()
         self.assertEqual(len(tcp_ping.measures), 1)
 
     async def test_ping_domain_incorrect(self):
@@ -45,17 +44,16 @@ class TestCorrectPings(AioTestCase):
         args = cmd_parser.parse_args(['google.csom'])
         tcp_ping = ping.TCPing(destination=args.destination, port=args.port,
                                timeout=args.timeout, use_ipv6=args.use_ipv6)
-        pings_count = int(args.pings_count)
+
         with self.assertRaises(errors.InvalidIpOrDomain):
-            one_measure = await tcp_ping.do_ping()
+            await tcp_ping.do_ping()
 
     async def test_ping_ip_standart(self):
         cmd_parser = tcping.create_cmd_parser()
         args = cmd_parser.parse_args(['64.233.165.101', '-c', '10'])
         tcp_ping = ping.TCPing(destination=args.destination, port=args.port,
                                timeout=args.timeout, use_ipv6=args.use_ipv6)
-        pings_count = int(args.pings_count)
-        one_measure = await tcp_ping.do_ping()
+        await tcp_ping.do_ping()
         self.assertEqual(len(tcp_ping.measures), 1)
 
     async def test_ping_ip_incorrect(self):
@@ -63,9 +61,8 @@ class TestCorrectPings(AioTestCase):
         args = cmd_parser.parse_args(['64.233.165.101.123.214'])
         tcp_ping = ping.TCPing(destination=args.destination, port=args.port,
                                timeout=args.timeout, use_ipv6=args.use_ipv6)
-        pings_count = int(args.pings_count)
         with self.assertRaises(errors.InvalidIpOrDomain):
-            one_measure = await tcp_ping.do_ping()
+            await tcp_ping.do_ping()
 
 
 class TestParsing(unittest.TestCase):
@@ -113,10 +110,10 @@ class TestStatistics(unittest.TestCase):
         self.assertEqual(expected, ping_info)
 
     def test_statistics_all_successful(self):
-        excpected = "Statistic tcping for [127.0.0.1:123]:\n" \
-                    "Pings count: 4, Successful: 4, Failed: 0\n" \
-                    "Fails percentage: 0.0\n" \
-                    "Max time: 51.0ms, Min time: 49.0ms, Average time 49.75ms\n"
+        expected = "Statistic tcping for [127.0.0.1:123]:\n" \
+                   "Pings count: 4, Successful: 4, Failed: 0\n" \
+                   "Fails percentage: 0.0\n" \
+                   "Max time: 51.0ms, Min time: 49.0ms, Average time 49.75ms\n"
 
         measures = []
         single_stat = ping.StatisticsData(0.051, ip='127.0.0.1', port=123)
@@ -128,13 +125,13 @@ class TestStatistics(unittest.TestCase):
         single_stat = ping.StatisticsData(0.050, ip='127.0.0.1', port=123)
         measures.append(single_stat)
         statstics = statistics.Statistics(measures, '127.0.0.1', '123')
-        self.assertEqual(excpected, statstics.__str__())
+        self.assertEqual(expected, statstics.__str__())
 
     def test_statistics_all_failed(self):
-        excpected = "Statistic tcping for [127.0.0.1:123]:\n" \
-                    "Pings count: 4, Successful: 0, Failed: 4\n" \
-                    "Fails percentage: 100.0\n" \
-                    "Max time: 0.0ms, Min time: 0.0ms, Average time 0.0ms\n"
+        expected = "Statistic tcping for [127.0.0.1:123]:\n" \
+                   "Pings count: 4, Successful: 0, Failed: 4\n" \
+                   "Fails percentage: 100.0\n" \
+                   "Max time: 0.0ms, Min time: 0.0ms, Average time 0.0ms\n"
 
         measures = []
         single_stat = ping.StatisticsData(-1, ip='127.0.0.1', port=123)
@@ -146,13 +143,13 @@ class TestStatistics(unittest.TestCase):
         single_stat = ping.StatisticsData(-1, ip='127.0.0.1', port=123)
         measures.append(single_stat)
         statstics = statistics.Statistics(measures, '127.0.0.1', '123')
-        self.assertEqual(excpected, statstics.__str__())
+        self.assertEqual(expected, statstics.__str__())
 
     def test_statistics_some_failed(self):
-        excpected = "Statistic tcping for [127.0.0.1:123]:\n" \
-                    "Pings count: 4, Successful: 2, Failed: 2\n" \
-                    "Fails percentage: 50.0\n" \
-                    "Max time: 50.0ms, Min time: 50.0ms, Average time 50.0ms\n"
+        expected = "Statistic tcping for [127.0.0.1:123]:\n" \
+                   "Pings count: 4, Successful: 2, Failed: 2\n" \
+                   "Fails percentage: 50.0\n" \
+                   "Max time: 50.0ms, Min time: 50.0ms, Average time 50.0ms\n"
 
         measures = []
         single_stat = ping.StatisticsData(0.050, ip='127.0.0.1', port=123)
@@ -164,7 +161,7 @@ class TestStatistics(unittest.TestCase):
         single_stat = ping.StatisticsData(-1, ip='127.0.0.1', port=123)
         measures.append(single_stat)
         statstics = statistics.Statistics(measures, '127.0.0.1', '123')
-        self.assertEqual(excpected, statstics.__str__())
+        self.assertEqual(expected, statstics.__str__())
 
     def test_statistics_data_is_float(self):
         single_stat = ping.StatisticsData(0.051, ip='127.0.0.1', port=123)
@@ -184,17 +181,32 @@ class TestStatistics(unittest.TestCase):
 
 class TestWatchdog(unittest.TestCase):
     def test_parse_destinations(self):
-        raw_destinations = ['google.com', 'google.com:443', '127.0.0.1', '127.0.0.1:123']
-        expected_destinations = [('google.com', "80"), ('google.com', "443"), ('127.0.0.1', "80"), ('127.0.0.1', "123")]
-        parsed_destinations = watchdog_ping.WatchdogPingData.parse_destinations(raw_destinations)
+        raw_destinations = ['google.com',
+                            'google.com:443',
+                            '127.0.0.1',
+                            '127.0.0.1:123']
+        expected_destinations = [('google.com', "80"),
+                                 ('google.com', "443"),
+                                 ('127.0.0.1', "80"),
+                                 ('127.0.0.1', "123")]
+        parsed_destinations = watchdog_ping \
+            .WatchdogPingData.parse_destinations(raw_destinations)
         self.assertEqual(expected_destinations, parsed_destinations)
 
     def test_init(self):
-        raw_destinations = ['google.com', 'google.com:443', '127.0.0.1', '127.0.0.1:123']
-        parsed_destinations = watchdog_ping.WatchdogPingData.parse_destinations(raw_destinations)
-        watchdog_ping_object = watchdog_ping.WatchdogPingData(destinations=parsed_destinations, timeout=1,
-                                                              use_ipv6=True)
-        self.assertEqual(watchdog_ping_object.destinations, parsed_destinations)
+        raw_destinations = ['google.com',
+                            'google.com:443',
+                            '127.0.0.1',
+                            '127.0.0.1:123']
+        parsed_destinations = watchdog_ping \
+            .WatchdogPingData \
+            .parse_destinations(raw_destinations)
+        watchdog_ping_object = watchdog_ping \
+            .WatchdogPingData(destinations=parsed_destinations,
+                              timeout=1,
+                              use_ipv6=True)
+        self.assertEqual(watchdog_ping_object.destinations,
+                         parsed_destinations)
         self.assertEqual(watchdog_ping_object.timeout, 1)
         self.assertEqual(watchdog_ping_object.use_ipv6, True)
 
@@ -206,11 +218,21 @@ class TestWatchdog(unittest.TestCase):
 
     def test_get_pings(self):
         raw_destinations = ['google.com', 'google.com:443']
-        pings_expected = [ping.TCPing(destination='google.com', port=80, use_ipv6=False, timeout=1),
-                          ping.TCPing(destination='google.com', port=443, use_ipv6=False, timeout=1)]
-        parsed_destinations = watchdog_ping.WatchdogPingData.parse_destinations(raw_destinations)
-        watchdog_ping_object = watchdog_ping.WatchdogPingData(destinations=parsed_destinations, timeout=1,
-                                                              use_ipv6=False)
+        pings_expected = [ping.TCPing(destination='google.com',
+                                      port=80,
+                                      use_ipv6=False,
+                                      timeout=1),
+                          ping.TCPing(destination='google.com',
+                                      port=443,
+                                      use_ipv6=False,
+                                      timeout=1)]
+        parsed_destinations = watchdog_ping \
+            .WatchdogPingData \
+            .parse_destinations(raw_destinations)
+        watchdog_ping_object = watchdog_ping \
+            .WatchdogPingData(destinations=parsed_destinations,
+                              timeout=1,
+                              use_ipv6=False)
         pings = watchdog_ping_object.get_pings()
         self.assertEqual(len(pings_expected), len(pings))
         self.assertEqual(type(pings[0]), ping.TCPing)
@@ -225,36 +247,58 @@ class TestWatchdog(unittest.TestCase):
 |   meow.org  | 192.167.2.1 | 123  |    -    |   Closed  |
 +-------------+-------------+------+---------+-----------+"""
         measures_with_dest = []
-        single_stat = ping.StatisticsData(0.050, ip='127.0.0.1', port=123), "hemlo.com"
+        single_stat = ping.StatisticsData(0.050,
+                                          ip='127.0.0.1',
+                                          port=123), "hemlo.com"
         measures_with_dest.append(single_stat)
-        single_stat = ping.StatisticsData(0.050, ip='192.168.0.2', port=443), "google.com"
+        single_stat = ping.StatisticsData(0.050,
+                                          ip='192.168.0.2',
+                                          port=443), "google.com"
         measures_with_dest.append(single_stat)
-        single_stat = ping.StatisticsData(-1, ip='192.167.2.1', port=123), "meow.org"
+        single_stat = ping.StatisticsData(-1,
+                                          ip='192.167.2.1',
+                                          port=123), "meow.org"
         measures_with_dest.append(single_stat)
 
-        measures_to_print = watchdog_ping.WatchdogPingData.get_measures_to_print(measures_with_dest)
-        self.assertEqual(measures_to_print_expected, measures_to_print.__str__())
+        measures_to_print = watchdog_ping \
+            .WatchdogPingData \
+            .get_measures_to_print(measures_with_dest)
+        self.assertEqual(measures_to_print_expected,
+                         measures_to_print.__str__())
 
     def test_create_tasks(self):
         raw_destinations = ['google.com', 'google.com:443']
-        parsed_destinations = watchdog_ping.WatchdogPingData.parse_destinations(raw_destinations)
-        watchdog_ping_object = watchdog_ping.WatchdogPingData(destinations=parsed_destinations, timeout=1,
-                                                              use_ipv6=False)
+        parsed_destinations = watchdog_ping \
+            .WatchdogPingData \
+            .parse_destinations(raw_destinations)
+        watchdog_ping_object = watchdog_ping \
+            .WatchdogPingData(destinations=parsed_destinations,
+                              timeout=1,
+                              use_ipv6=False)
         pings = watchdog_ping_object.get_pings()
-        tasks = watchdog_ping_object.create_tasks_from_pings(pings, ioloop=asyncio.get_event_loop())
+        tasks = watchdog_ping_object \
+            .create_tasks_from_pings(pings,
+                                     ioloop=asyncio.get_event_loop())
         self.assertIsInstance(tasks, list)
         self.assertGreater(len(tasks), 0)
         self.assertIsInstance(tasks[0], _asyncio.Task)
 
     def test_min_max(self):
         measures_with_dest = []
-        single_stat = ping.StatisticsData(0.050, ip='127.0.0.1', port=123), "hemlo.com"
+        single_stat = ping.StatisticsData(0.050,
+                                          ip='127.0.0.1',
+                                          port=123), "hemlo.com"
         measures_with_dest.append(single_stat)
-        single_stat = ping.StatisticsData(0.059, ip='192.168.0.2', port=443), "google.com"
+        single_stat = ping.StatisticsData(0.059,
+                                          ip='192.168.0.2',
+                                          port=443), "google.com"
         measures_with_dest.append(single_stat)
-        single_stat = ping.StatisticsData(-1, ip='192.167.2.1', port=123), "meow.org"
+        single_stat = ping.StatisticsData(-1,
+                                          ip='192.167.2.1',
+                                          port=123), "meow.org"
         measures_with_dest.append(single_stat)
-        max, min = watchdog_ping.WatchdogPingData.get_max_and_min_time(measures_with_dest)
+        max, min = watchdog_ping.WatchdogPingData \
+            .get_max_and_min_time(measures_with_dest)
         self.assertEqual(max, 0.059)
         self.assertEqual(min, -1)
 
